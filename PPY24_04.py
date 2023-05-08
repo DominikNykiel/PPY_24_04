5f8138ceb01607cbfc9613e6ae94c0b16129a5ff
DominikNykiel <s24813@pjwstk.edu.pl>
2023-05-08 22:42:19 +0200

    Student class almost done

--- PPY24_04.py
+++ PPY24_04.py
@@ -8,4 +8,14 @@ myList.add_node("bbbbb")
 myList.add_node("aaaa")
 myList.add_node("cccc")
 
+print("Lista po dodaniu elementów:")
+print(myList)
+
+myList.delete("aaaa")
+
+print("Lista po usunięciu elementów:")
+print(myList)
+
+
+
 
--- Student.py
+++ Student.py
@@ -26,5 +26,23 @@ class Student:
         return "Email: " + self.email + "\nName: " + self.name + "\nSurname: " + self.surname + "\nAll grades: " + str(
             self.all_grade) + "\nStatus: " + str(self.status)
 
+    def __eq__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+        return self.email == other.email
 
+    def __repr__(self):
+        return self.__str__()
+
+    @staticmethod
+    def compareStudentsNames(x, y):
+        return x.name >= y.name
+
+    @staticmethod
+    def compareStudentsSurname(x, y):
+        return x.surname >= y.surname
+
+    @staticmethod
+    def getPersonalData(self):
+        return self.email, self.name, self.surname
 
