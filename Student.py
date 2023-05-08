b453b97dca1378b4adfd02cf592a3e629711e969
DominikNykiel <127125171+DominikNykiel@users.noreply.github.com>
2023-05-08 22:47:12 +0200

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,22 +1,6 @@
 class Student:
-    def __init__(self, email, name, surname):
-        self.all_grade = {
-            "project": -1,
-            "l_1": -1,
-            "l_2": -1,
-            "l_3": -1,
-            "h_1": -1,
-            "h_2": -1,
-            "h_3": -1,
-            "h_4": -1,
-            "h_5": -1,
-            "h_6": -1,
-            "h_7": -1,
-            "h_8": -1,
-            "h_9": -1,
-            "h_10": -1,
-            "grade": -1,
-        }
+    def __init__(self, all_grade, email, name, surname):
+        self.all_grade = all_grade
         self.email = email
         self.name = name
         self.surname = surname
